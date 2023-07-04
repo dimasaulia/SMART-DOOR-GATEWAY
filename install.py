4f547f17be2aa8f039b469b3766ab93a1de7a880
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2023-07-04 09:40:06 +0700

    add executable app

--- install.py
+++ install.py
@@ -1,10 +1,48 @@
 
-# INSTALL VIRTUAL ENV
+import os
+import platform
+import subprocess
+import time
+OS = str(platform.system()).upper()
 
-# INSTALL ALL LIBRARY
+def isFileExist (path):
+    if os.path.isfile(path):
+        return True
+    else:
+        return False
+
+def isDirectoryExist (path):
+    if os.path.isdir(path):
+        return True
+    else:
+        return False
+
+# ACTIVATE THE venv
+if OS == "WINDOWS":
+    # Install packages from requirements.txt
+    subprocess.call('pip install -r requirements.txt', shell=True)
+
+if OS == "LINUX":
+    # Install packages from requirements.txt
+    subprocess.call('pip install -r rpi.txt', shell=True)
 
 # CHECK DATABASE
+if(isFileExist("./database/gateway.db") == True):
+    print("DATABASE ALREADY EXIST")
 
 # IF EMPTY MIGRATE
+if(isFileExist("./database/gateway.db") == False):
+    subprocess.call('python ./database/migrate.py', shell=True)
 
 # CREATE EXECUTEABLE APP
+import PyInstaller.__main__
+
+# Specify the path to your Python script
+script_path = './run.py'
+
+# Set PyInstaller options
+options = ['--onefile', '--distpath', './', '--name', 'gateway']
+
+# Build the executable
+PyInstaller.__main__.run([*options, script_path])
+
