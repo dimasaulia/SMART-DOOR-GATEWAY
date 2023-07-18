205e6d6b824f760084d457bdc3b178123d70e4d5
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2023-07-18 08:43:34 +0700

    run new script from the same terminal

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ OS = str(platform.system()).upper()
 
 if __name__ == '__main__':
     if OS == "WINDOWS":
-        subprocess.run(['python', 'main.py'])
+        subprocess.run(['venv/Scripts/python', 'main.py'])
     if OS == "LINUX":
         subprocess.run(['venv/bin/python', 'main.py'])
