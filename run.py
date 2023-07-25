2adc58831ceaea24d96980ca63017ef92fd45659
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2023-07-25 19:54:44 +0700

    (fix): Fix problem with relative path not start script

--- run.py
+++ run.py
@@ -4,10 +4,6 @@ OS = str(platform.system()).upper()
 
 if __name__ == '__main__':
     if OS == "WINDOWS":
-        import os
-        import sys
-        venv_python = os.path.join(os.path.dirname(__file__), 'venv', 'Scripts', 'python.exe')
-        venv_python = sys.executable
-        subprocess.run(f"{venv_python} main.py")
+        subprocess.run(['venv/Scripts/python', 'main.py'])
     if OS == "LINUX":
         subprocess.run(['venv/bin/python', 'main.py'])
